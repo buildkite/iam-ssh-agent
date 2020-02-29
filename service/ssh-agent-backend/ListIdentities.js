0eced924e32c0a667e6629443adaced4a45dbbdc
Keith Duncan <keith_duncan@me.com>
2020-02-29 18:08:22 +1100

    Rename the source package to ssh-agent-backend

--- service/ssh-agent-backend/ListIdentities.js
+++ service/ssh-agent-backend/ListIdentities.js
@@ -14,7 +14,7 @@ let response;
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
-exports.lambdaHandler = async (event, context) => {
+exports.handler = async (event, context) => {
     try {
         // const ret = await axios(url);
         response = {
