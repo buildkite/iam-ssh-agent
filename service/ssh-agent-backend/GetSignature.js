1198da46e89402d80cc63a5f64cc413142e5cd95
Keith Duncan <keith_duncan@me.com>
2020-03-01 20:15:44 +1100

    More work on the GetSignature lambda

--- service/ssh-agent-backend/GetSignature.js
+++ service/ssh-agent-backend/GetSignature.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
 
-import fetchKeyParametersListForCaller from './lib.js';
+import { fetchKeyParametersListForCaller, fetchPublicKeyForParameter } from './lib.js';
 
-async function fetchPrivateKey(key) {
+async function fetchPrivateKeyForParameter(keyParameter) {
     let ssm = new AWS.SSM({apiVersion: '2014-11-06'});
 
     let response = await ssm.getParameter({
-        Name: key,
+        Name: keyParameter,
         WithDecryption: true,
     }).promise();
 
@@ -19,13 +19,33 @@ exports.handler = async (event, context) => {
         let [caller,_] = identity.caller.split(":");
         console.log(`fn=handler caller=${caller}`);
 
+        let { KeyBlob, Data, Flags } = JSON.parse(event.body);
+
+        // Find the parameter that stores the private/public key pair for blob
+        // searching the list of keys the caller has access to.
         let keyList = await fetchKeyParametersListForCaller(caller);
         console.log(`fn=handler caller=${caller} keys=${keyList.join(',')}`);
+
+        for (const keyParameter of keyList) {
+            let publicKey = await fetchPublicKeyForParameter(keyParameter);
+            if (publicKey != KeyBlob) {
+                continue;
+            }
+
+            let privateKey = fetchPrivateKeyForParameter(keyParameter);
+
+            return {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    signature: "",
+                }),
+            }
+        }
         
         return {
-            'statusCode': 200,
+            'statusCode': 404,
             'body': JSON.stringify({
-                signature: "",
+                message: "key blob not found in list of keys caller has access to",
             })
         }
     } catch (err) {
