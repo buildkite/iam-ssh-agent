77383093ff7ed2a8e14cc81143af0d0b9a8f02e8
Keith Duncan <keith_duncan@me.com>
2020-03-03 12:13:10 +1100

    Add ssh2-streams to parse the keys

--- service/ssh-agent-backend/GetSignature.js
+++ service/ssh-agent-backend/GetSignature.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 
 const lib = require('./lib.js');
+const ssh2 = require('./node_modules/ssh2-streams/index.js');
 
 async function fetchPrivateKeyForParameter(keyParameter) {
     let ssm = new AWS.SSM({apiVersion: '2014-11-06'});
@@ -15,11 +16,16 @@ async function fetchPrivateKeyForParameter(keyParameter) {
 
 exports.handler = async (event, context) => {
     try {
+        console.log(`fn=handler event=${JSON.stringify(event)}`);
+
         let identity = event.requestContext.identity;
         let [caller,_] = identity.caller.split(":");
         console.log(`fn=handler caller=${caller}`);
 
         let { pubkey, data, flags } = JSON.parse(event.body);
+
+        let decoded_pubkey = Buffer.from(pubkey, 'base64');
+        let decoded_data = Buffer.from(data, 'base64');
 
         // Find the parameter that stores the private/public key pair for blob
         // searching the list of keys the caller has access to.
@@ -28,14 +34,19 @@ exports.handler = async (event, context) => {
 
         for (const keyParameter of keyList) {
             let key = await lib.fetchPublicKeyForParameter(keyParameter);
+            let parsed_key = ssh2.utils.parseKey(key);
 
-            // pubkey is base64(pubkey bits), key is a string rep of the public key with comment etc
-            // this needs to parse key and compare the key bits
-            if (pubkey != key) {
+            // pubkey is base64(pubkey bits)
+            // decoded_pubkey is binary key bits
+            //
+            // key is a string rep of the public key with comment etc
+            // parsed_key is an OpenSSH key from ssh2-streams
+            if (decoded_pubkey != parsed_key.getPublicSSH()) {
+                console.log(`fn=handler caller=${caller} key=${keyParameter} at=skip`);
                 continue;
             }
-
             console.log(`fn=handler caller=${caller} key=${keyParameter} at=match`);
+            
             let privateKey = fetchPrivateKeyForParameter(keyParameter);
 
             return {
