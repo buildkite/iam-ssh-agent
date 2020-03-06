7fb9f93096c2a707ab4727b0c50e20ece374599b
Keith Duncan <keith_duncan@me.com>
2020-03-06 12:53:42 +1100

    Fetch keys using SSM.getParameters

--- service/ssh-agent-backend/lib.js
+++ service/ssh-agent-backend/lib.js
@@ -28,6 +28,16 @@ exports.fetchKeyParameterListForCaller = async (caller) => {
     return parameters.SS;
 };
 
+function replaceKeyCommentWithParameterArn(key, arn) {
+    let components = key.split(' ');
+    if (components.length == 3) {
+        components.pop();
+    }
+    components.push(arn);
+
+    return components.join(' ');
+}
+
 async function fetchPublicKeyForParameter(keyParameter) {
     let ssm = new AWS.SSM({apiVersion: '2014-11-06'});
 
@@ -35,21 +45,37 @@ async function fetchPublicKeyForParameter(keyParameter) {
         Name: `${keyParameter}.pub`,
     }).promise();
 
-    let value = response.Parameter.Value;
-
-    let components = value.split(' ');
-    if (components.length == 3) {
-        components.pop();
-    }
-    components.push(response.Parameter.ARN);
-
-    return components.join(' ');
+    return replaceKeyCommentWithParameterArn(response.Parameter.Value, response.Parameter.ARN);
 }
 
 exports.fetchPublicKeyForParameter = fetchPublicKeyForParameter;
 
+function* chunkArrayInGroups(arr, size) {
+  for (var i=0; i<arr.length; i+=size)
+    yield arr.slice(i, i+size);
+}
+
 exports.fetchPublicKeysForParameters = async (keyParameters) => {
-    Promise.all(keyParameters.map((keyParameter) => {
-        fetchPublicKeyForParameter(keyParameter)
+    let ssm = new AWS.SSM({apiVersion: '2014-11-06'});
+
+    // Fetch up to 10 parameters at a time
+    let groups = chunkArrayInGroups(keyParameters.map((keyParameter) => {
+        `${keyParameter}.pub`
+    }), 10);
+
+    let responses = await Promise.all(groups.map((keyParameters) => {
+        return ssm.getParameters({
+            Names: keyParameters,
+        })
     }));
+
+    let parameters = responses
+        .map((response) => {
+            response.Parameters
+        })
+        .flat();
+
+    return parameters.map((parameter) => {
+        return replaceKeyCommentWithParameterArn(parameter.Value, parameter.ARN);
+    });
 };
