0e30feb1c854bde8569ab4f455a30e450c4ced8d
Keith Duncan <keith_duncan@me.com>
2020-04-20 17:22:35 +1000

    Support reading SSH_AGENT_SOCK for the bind-to arg

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::Path};
+use std::{env, fs, path::Path};
 use ssh_agent::agent::Agent;
 use clap::{App, Arg, SubCommand};
 use url::Url;
@@ -28,14 +28,13 @@ fn main() {
 				.long("bind-to")
 				.short("b")
 				.takes_value(true)
-				.value_name("PATH")
-				.required(true)))
+				.value_name("PATH")))
 		.setting(clap::AppSettings::SubcommandRequired)
 		.get_matches();
 
 	// Uses an environment variable rather than an argument so that this can be
 	// an ECS ValueFrom in an ECS task.
-	let ssh_agent_backend_url = Url::parse(&std::env::var("IAM_SSH_AGENT_BACKEND_URL").expect("IAM_SSH_AGENT_BACKEND_URL is required")).expect("IAM_SSH_AGENT_BACKEND_URL is a valid url");
+	let ssh_agent_backend_url = Url::parse(&env::var("IAM_SSH_AGENT_BACKEND_URL").expect("IAM_SSH_AGENT_BACKEND_URL is required")).expect("IAM_SSH_AGENT_BACKEND_URL is a valid url");
 	let agent = agent::Backend::new(ssh_agent_backend_url);
 
 	if let Some(_matches) = matches.subcommand_matches("list-keys") {
@@ -46,8 +45,8 @@ fn main() {
 	if let Some(matches) = matches.subcommand_matches("daemon") {
 		// TODO support exec mode and export SSH_AUTH_SOCK
 
-		let pipe = matches.value_of("bind-to").expect("bind-to is required");
-        let pipe = Path::new(pipe);
+		let pipe: String = matches.value_of("bind-to").map(str::to_string).or(env::var("SSH_AUTH_SOCK").ok()).expect("bind-to is required");
+        let pipe = Path::new(&pipe);
 
         if fs::metadata(&pipe).is_ok() {
             if let Ok(_) = fs::remove_file(&pipe){
